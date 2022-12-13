999516dc642be32df4b7f1512b6ff87dba696ffb
vikaashpanchbikesan <117252370+vikaashpanchbikesan@users.noreply.github.com>
2022-12-13 12:34:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  recognition.start();
+    
+} 
+
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+
+
+function speak(){
+
+    
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    setTimeout(function()
+    {
+        img_id = "selfie_1";
+        take_snapshot();
+        speak_data = "taking your selfie in 5 seconds";
+        var utterThis = new  SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+        
+    } , 5000 );
+}
+
+function take_snapshot()
+{
+    console.log(img_id);
+
+    Webcam.snap(function(data_uri)
+    {
+        if(img_id == "selfie1")
+        {
+            document.getElementById("reult1").innerHTML = '<img id="selfie1" src="'+data_uri+'"/>';
+        }
+
+        if(img_id == "selfie2")
+        {
+            document.getElementById("reult2").innerHTML = '<img id="selfie2" src="'+data_uri+'"/>';
+        }
+
+        if(img_id == "selfie3")
+        {
+            document.getElementById("reult3").innerHTML = '<img id="selfie3" src="'+data_uri+'"/>';
+        }
+    });
+
+}
